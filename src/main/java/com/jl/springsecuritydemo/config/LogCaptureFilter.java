72ba875f0937542b2d397db8dba9f2fa7573b5d8
JL <thoughtworks.t.li@extaccount.com>
2024-03-05 15:21:37 +0800

    jl feat: add custom SecurityFilterChain and don't use @EnableWebSecurity

---
+++ src/main/java/com/jl/springsecuritydemo/config/LogCaptureFilter.java
@@ -0,0 +1,19 @@
+package com.jl.springsecuritydemo.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+public class LogCaptureFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) {
+    log.info(String.format("request with url %s received", request.getRequestURL()));
+    }
+}
--- src/main/java/com/jl/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/jl/springsecuritydemo/config/WebSecurityConfig.java
@@ -1,32 +1,23 @@
 package com.jl.springsecuritydemo.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.List;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity(debug = true)
 public class WebSecurityConfig {
 
     @Bean
-    public UserDetailsService loadInMemoryUserDetailsService(){
-        UserDetails user1 = User.withUsername("user1").password("12345").roles("Admin").build();
-        UserDetails user2 = User.withUsername("user2").password("hhhhh123").roles("User").build();
-        return new InMemoryUserDetailsManager(List.of(user1, user2));
-    }
-
-    @Bean
-    public PasswordEncoder loadNoPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/custom/**").authenticated()
+                                .requestMatchers("/default/hello").permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
