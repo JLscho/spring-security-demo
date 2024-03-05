a8468e972cd765d77147cccdc2e740749c4abd0f
JL <thoughtworks.t.li@extaccount.com>
2024-03-05 16:38:23 +0800

    jl feat: change default AuthenticationProvider behavior

--- src/main/java/com/jl/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/jl/springsecuritydemo/config/WebSecurityConfig.java
@@ -4,8 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 public class WebSecurityConfig {
@@ -17,8 +25,21 @@ public class WebSecurityConfig {
                         request.requestMatchers("/custom/**").authenticated()
                                 .requestMatchers("/default/**").permitAll()
                 )
+                //inject default BasicAuthenticationFilter
                 .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(new LogCaptureFilter(), BasicAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService loadMyInMemoryUserDetailsService() {
+        UserDetails user1 = User.builder().username("testUser1").password("12345").build();
+        UserDetails user2 = User.builder().username("testUser2").password("test12345").build();
+        return new InMemoryUserDetailsManager(List.of(user1, user2));
+    }
+
+    @Bean
+    public PasswordEncoder loadPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
